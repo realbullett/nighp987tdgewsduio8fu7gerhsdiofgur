3c28f2ee5eac67b5f3e8da39d0cd1dba6000f0ef
realbullett <asukalistpositive@gmail.com>
2026-01-29 23:09:37 +0300

    yahaaaha

--- api/status.ts
+++ api/status.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ywvuugjhvyuooskaehrc.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_lXKhuTl9phDTUoc39GkIpw_az63tUKQ';
+const SUPABASE_URL = 'https://txpmghnjcaoojmomxhry.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_tBAnI45AOKPD3_NpEDqeZw_75uBZxwh';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
